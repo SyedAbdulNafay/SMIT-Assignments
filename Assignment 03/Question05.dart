c972a5c7b6824453617cb840919eb1926359e246
SyedAbdulNafay <sabdulnafay6@gmail.com>
2024-02-10 19:00:36 +0500

    q5 update

--- Assignment 03/Question05.dart
+++ Assignment 03/Question05.dart
@@ -1,15 +1,12 @@
 import 'dart:io';
 void main(){
-  stdout.write("Enter your number here: ");
-  int input = int.parse(stdin.readLineSync() ?? '0');
-  String inputAsString = input.toString();
-  int index = 0;
+  stdout.write("Enter number: ");
+  String str_num = stdin.readLineSync() ?? '0';
   int sum = 0;
-  
-  while (index < inputAsString.length) {
-    int digit = int.parse(inputAsString[index]);
-    sum = sum + digit;
-    index++;
+
+  for (var i = 0; i < str_num.length; i++) {
+    sum += int.parse(str_num[i]);
   }
+
   print(sum);
 }
